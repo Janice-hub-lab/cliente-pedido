8057b4034a7892524faff82dc8e99bba200bb68d
Jane Gomes <asscontecjane@gmail.com>
2024-10-26 01:38:01 -0700

    implementa request e response

--- src/main/java/dev/wakandaacademy/cliente_pedido/cliente/application/api/ClienteRequest.java
+++ src/main/java/dev/wakandaacademy/cliente_pedido/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,22 @@
 package dev.wakandaacademy.cliente_pedido.cliente.application.api;
 
-public class ClienteRequest {
+import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	private String nomeCompleto;
+    @NotBlank
+    private String endereco;
+    @NotBlank
+    private String celular;
+    @NotBlank
+    @CPF
+    private String cpf;
+    @NotBlank
+    @Email
+    private String email;
 }

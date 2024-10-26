8d531935f428b9a151da026aa4293f5c5ae3c2db
Jane Gomes <asscontecjane@gmail.com>
2024-10-26 12:52:22 -0700

    implementa getTodos

--- src/main/java/dev/wakandaacademy/cliente_pedido/cliente/application/api/ClienteListResponse.java
+++ src/main/java/dev/wakandaacademy/cliente_pedido/cliente/application/api/ClienteListResponse.java
@@ -2,18 +2,31 @@ package dev.wakandaacademy.cliente_pedido.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import dev.wakandaacademy.cliente_pedido.cliente.domain.Cliente;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
-    private UUID idCliente;
-    private String nomeCompleto;
-    private String cpf;
-    private String celular;
-    private String email;
-    
-    public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String celular;
+	private String email;
+
+	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
+	}
+
+	private ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.cpf = cliente.getCpf();
+		this.celular = cliente.getCelular();
+		this.email = cliente.getEmail();
 	}
 
 }

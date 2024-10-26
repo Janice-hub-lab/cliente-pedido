3a3423b653e7a27e860072dc114fb5f065de177b
Jane Gomes <asscontecjane@gmail.com>
2024-10-26 00:28:16 -0700

    ajusta mapeamento uuid

--- src/main/java/dev/wakandaacademy/cliente_pedido/cliente/domain/Cliente.java
+++ src/main/java/dev/wakandaacademy/cliente_pedido/cliente/domain/Cliente.java
@@ -12,10 +12,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Entity
 public class Cliente {
@@ -38,4 +39,17 @@ public class Cliente {
 
     private LocalDateTime dataHoraDoCadastro;
     private LocalDateTime getDataHoraDaUltimaAlteracao;
+    
+	public Cliente(UUID idCliente, @NotBlank String nomeCompleto, @NotBlank String endereco, @NotBlank String celular,
+			@NotBlank @CPF String cpf, @NotBlank @Email String email) {
+		this.nomeCompleto = nomeCompleto;
+		this.endereco = endereco;
+		this.celular = celular;
+		this.cpf = cpf;
+		this.email = email;
+		this.dataHoraDoCadastro = LocalDateTime.now();
+	}
+	
+    
 }
+

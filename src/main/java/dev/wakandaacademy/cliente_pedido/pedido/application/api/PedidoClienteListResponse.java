829bfe93ae7dfe8794a14cf1c6a5462394dba8d9
Jane Gomes <asscontecjane@gmail.com>
2024-10-28 11:34:15 -0700

    implementa conversao pedido para pedidoClienteResponse

--- src/main/java/dev/wakandaacademy/cliente_pedido/pedido/application/api/PedidoClienteListResponse.java
+++ src/main/java/dev/wakandaacademy/cliente_pedido/pedido/application/api/PedidoClienteListResponse.java
@@ -1,13 +1,32 @@
 package dev.wakandaacademy.cliente_pedido.pedido.application.api;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import dev.wakandaacademy.cliente_pedido.cliente.application.api.ClienteListResponse;
 import dev.wakandaacademy.cliente_pedido.pedido.domain.Pedido;
+import lombok.Value;
 
+@Value
 public class PedidoClienteListResponse {
+	private UUID idPedido;
+	private UUID idCliente;
+	private String quantidade;
+	private String descricao;
+	private String metodoPagamento;
 
 	public static List<PedidoClienteListResponse> converte(List<Pedido> pedidosDoCliente) {
-		return null;
+		return pedidosDoCliente.stream()
+				.map(PedidoClienteListResponse::new)
+				.collect(Collectors.toList());
 	}
 
+	public PedidoClienteListResponse(Pedido pedido) {
+		this.idPedido = pedido.getIdPedido();
+		this.idCliente = pedido.getIdCliente();
+		this.quantidade = pedido.getQuantidade();
+		this.descricao = pedido.getDescricao();
+		this.metodoPagamento = pedido.getMetodoPagamento();
+	}
 }
